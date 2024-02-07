bdf4b94114ce871fcdeac464c192a46e33c806b1
yolandahe <he.yang7@northeastern.edu>
2024-02-06 22:38:36 -0800

    added styling for screen header and tab bars

--- App.js
+++ App.js
@@ -1,30 +1,42 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Button, Pressable, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome6 } from '@expo/vector-icons';
 
 import AddActivity from './screens/AddActivity';
 import AllActivities from './screens/AllActivities';
 import SpecialActivities from './screens/SpecialActivities';
 import Start from './screens/Start';
+import CommonStyles from './styles/CommonStyles';
 
-//create nested navigation of two bottom tabs 
-//(all and special activities in home screen)
+//nested navigation of two bottom tabs in home screen
 const Tab = createBottomTabNavigator();
 const MyTabs = () => {
   return (
     <Tab.Navigator
       screenOptions={({navigation, route}) => ({
-        headerRight:() => (
-          <Pressable onPress={() => navigation.navigate("Add")}>
-            <Text size={30} color="orange">Add </Text>
-          </Pressable>
-        )
+        headerStyle: CommonStyles.purpleDark,
+        headerTintColor: "#fff",
+        headerTitleAlign:"center",
+        tabBarStyle: CommonStyles.purpleDark,
+        tabBarActiveTintColor: "rgb(235,187,66)",
+        headerRight:() => {
+          return <Button title="Add" color="orange" onPress={() => {navigation.navigate('Add')}} />
+        },
+        tabBarIcon:() => {
+          let iconName;
+          if (route.name === 'All Activities') {
+            iconName ='dollar'; // Icon name for "All" tab
+          } else if (route.name === 'Special Activities') {
+            iconName ='exclamation'; // Icon name for "Special" tab
+          }
+          return <FontAwesome6 name={iconName} size={24} color="orange" />;
+        }
       })}
     >  
       <Tab.Screen name="All Activities" component={AllActivities} />
-      <Tab.Screen name="Special Acitivities" component={SpecialActivities} />
+      <Tab.Screen name="Special Activities" component={SpecialActivities} />
     </Tab.Navigator>
   )
 }
@@ -34,7 +46,12 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions = {{
+          headerStyle: CommonStyles.purpleDark,
+          headerTintColor:"#fff",
+          headerTitleAlign:'center',
+        }}>
         <Stack.Screen 
           name="Start"
           component={Start}
