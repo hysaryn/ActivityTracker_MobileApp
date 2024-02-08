98f752f7c0e26b8059a16aab2e5384527dcd6403
yolandahe <he.yang7@northeastern.edu>
2024-02-07 17:39:28 -0800

    added activitieslist component

--- components/ActivitiesList.js
+++ components/ActivitiesList.js
@@ -1,12 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import React, { useContext } from 'react'
+import ActivityContext from './ActivityContext'
 
-export default function ActivitiesList() {
-  return (
-    <View>
-      <Text>ActivitiesList</Text>
-    </View>
-  )
+export default function ActivitiesList({type}) {
+    const {data} = useContext(ActivityContext);
+
+    const filteredActivities = type === 'special' ? 
+    data.filter(activity => (activity.type === 'Running' || activity.type === 'Weight Training') && activity.duration > 60) :
+    data;
+
+    return (
+        <FlatList
+        data={filteredActivities}
+        renderItem={({item}) => (
+            <Text>
+                {item.type} - {item.data} - {item.duration} min
+            </Text>
+        )} />
+    )
 }
 
 const styles = StyleSheet.create({})
